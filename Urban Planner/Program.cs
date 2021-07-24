91cce1a3f878bc8974cf2b7aa91c82a4ae647fb6
John Maple <jmaple@bellsouth.net>
2021-07-24 12:22:29 -0500

    Add project files.

---
+++ Urban Planner/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Urban_Planner.Structures;
+
+namespace Urban_Planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo inputKey = new ConsoleKeyInfo();
+
+            string[] purchasers = new string[] { "Art Linkletter", "Bob Ross", "Homer Simpson", "Mr. Burns" };
+            List<Building> buildings = new List<Building>();
+
+            Console.WriteLine("\n\n               Welcome to the Urban Plannerl\n");
+            Console.WriteLine("               Creating Building Plans....");
+            Building OneTwelveBroad = new Building("112 Broadway");
+            Building OneTenChurch = new Building("110 Church St.");
+            Building SevenTwelveFourth = new Building("712 Fourth Ave. South");
+            Building OneTwentyBroad = new Building("120 Broadway");
+
+
+            Console.WriteLine($"               Constructing {OneTwelveBroad.Address()}");
+            OneTwelveBroad.Construct(20, 50.0, 75.0);
+            buildings.Add(OneTwelveBroad);
+            Console.WriteLine($"               Constructing {OneTenChurch.Address()}");
+            OneTenChurch.Construct(15, 45.0, 65.0);
+            buildings.Add(OneTenChurch);
+            Console.WriteLine($"               Constructing {SevenTwelveFourth.Address()}");
+            SevenTwelveFourth.Construct(40, 75.0, 130.0);
+            buildings.Add(SevenTwelveFourth);
+            Console.WriteLine($"               Constructing {OneTwentyBroad.Address()}");
+            OneTwentyBroad.Construct(35, 96.3, 140.7);
+            buildings.Add(SevenTwelveFourth);
+
+            for (int i = 0; i < buildings.Count; i ++)
+            {
+                Console.WriteLine($"               {purchasers[i]} is buying {buildings[i].Address()}");
+                buildings[i].Purchase(purchasers[i]);
+            }
+
+            for (int i = 0; i < buildings.Count; i++)
+            {
+                buildings[i].Status();
+                Console.Write('\n');
+            }
+            Console.WriteLine($"               Press any key to exit");
+            inputKey = Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ Urban Planner/Structures/Building.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urban_Planner.Structures
+{
+    class Building
+    {
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume { get; private set; }
+
+        string _designer;
+        DateTime _dateConstructed;
+        string _address;
+        string _owner;
+
+        public Building(string address)
+        {
+            _address = address;
+            _designer = "Bob Ross";
+            _dateConstructed = DateTime.MinValue;
+            _owner = null;
+            Width = 0.0;
+            Depth = 0.0;
+            Stories = 0;
+            Volume = 0;
+        }
+
+        public void Construct(int stories, double width, double depth)
+        {
+            _dateConstructed = DateTime.Now;
+            Stories = stories;
+            Width = width;
+            Depth = depth;
+            Volume = Width * Depth * (3 * Stories);
+        }
+
+        public void Purchase(string purchaser)
+        {
+            _owner = purchaser;
+        }
+
+        public void Status()
+        {
+            Console.WriteLine($"               Designed by {_designer}");
+            if (_dateConstructed != DateTime.MinValue)
+            {
+                Console.WriteLine($"               Consructed on {_dateConstructed}");
+            }
+            else
+            {
+                Console.WriteLine($"               This building has not yet been constructed.");
+
+            }
+            if (_owner != null)
+            {
+                Console.WriteLine($"               Owned by {_owner}");
+
+            }
+            else
+            {
+                Console.WriteLine($"               This building has not been purchased.");
+            }
+            if (_dateConstructed != DateTime.MinValue)
+            {
+                Console.WriteLine($"               {Volume} cubit meters of space.");
+            }
+
+        }
+        public string Address()
+        {
+            return _address;
+        }
+    }
+}
